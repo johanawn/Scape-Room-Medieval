69f69dcb58400201d29785bdfdd8a620add272a9
GABRIEL ANDRES BONILLA RIVERA <gabriel.bonillarivera@ucr.ac.cr>
2020-09-30 16:43:14 -0600

    Add new file

---
+++ TrianguloCartas.java
@@ -0,0 +1,64 @@
+public class TrianguloCartas{
+	
+public void Triangulo(int cartaElegida, int cartaEnemiga, int numeroMovimientos){
+	
+	for(int i = 1; i <= numeroMovimientos; i +=1){
+		
+	if(Mago.getVida() != 0){	
+	    if(cartaElegida == 0 && cartaEnemiga == 1){
+	      CartaEnemiga.setVida() = (CartaEnemiga.getVida() - Mago.getAtaque()) + CartaEnemiga.getDefensa();
+	    }
+	    else if(cartaElegida == 0 && cartaEnemiga == 0){
+	      CartaEnemiga.setVida() = (CartaEnemiga.getVida() - Mago.getAtaque()) + CartaEnemiga.getDefensa();
+	    }
+	    else if(cartaElegida == 0 && cartaEnemiga == 2){
+	      Mago.setVida() = (Mago.getVida() - CartaEnemiga.getAtaque()) + Mago.getDefensa();
+	    }
+	}
+	else{
+	
+	}
+	if (Espada.getVida() != 0){	
+	    else if(cartaElegida == 1 && cartaEnemiga == 2){
+	      CartaEnemiga.setVida() = (CartaEnemiga.getVida() - Espada.getAtaque()) + CartaEnemiga.getDefensa();
+	    }
+	    else if(cartaElegida == 1 && cartaEnemiga == 1){
+	      CartaEnemiga.setVida() = (CartaEnemiga.getVida() - Espada.getAtaque()) + CartaEnemiga.getDefensa();
+	    }
+	    else if(cartaElegida == 1 && cartaEnemiga == 0){
+	      Espada.setVida() = (Espada.getVida() - CartaEnemiga.getAtaque()) + Espada.getDefensa();
+	    }
+	}
+	else{
+	
+	}
+	if(Dragon.getVida != 0){		
+	    else if(cartaElegida == 2 && cartaEnemiga == 0){
+	      CartaEnemiga.setVida() = (CartaEnemiga.getVida() - Dragon.getAtaque()) + CartaEnemiga.getDefensa();
+	    }
+	    else if(cartaElegida == 2 && cartaEnemiga == 2){
+	      CartaEnemiga.setVida() = (CartaEnemiga.getVida() - Dragon.getAtaque()) + CartaEnemiga.getDefensa();
+	    }
+	    else if(cartaElegida == 2 && cartaEnemiga == 1){
+	      Dragon.setVida() = (Dragon.getVida() - CartaEnemiga.getAtaque()) + Dragon.getDefensa();
+	    }
+	}
+	else {
+	
+	}
+	
+	
+	
+	}
+	
+	
+	
+			
+
+	
+  
+}
+
+
+
+}
